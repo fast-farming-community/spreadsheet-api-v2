8db52d9653b043d16baf64f0f321f2c4df4a6cd4
Sam Wolfs <be.samwolfs@gmail.com>
2023-07-30 14:05:51 +0200

    Blep

--- lib/fast_api/application.ex
+++ lib/fast_api/application.ex
@@ -7,26 +7,30 @@ defmodule FastApi.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      {Goth, name: FastApi.Goth},
-      {Finch, name: FastApi.Finch},
-      FastApi.Repos.Fast,
-      FastApi.Scheduler,
-      # Start the Telemetry supervisor
-      FastApiWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: FastApi.PubSub},
-      # Start the Endpoint (http/https)
-      FastApiWeb.Endpoint,
-      # Start a worker by calling: FastApi.Worker.start_link(arg)
-      # {FastApi.Worker, arg}
-    ]
+    children =
+      List.flatten([
+        prod_only_processes(Mix.env()),
+        {Finch, name: FastApi.Finch},
+        FastApi.Repos.Fast,
+        FastApi.Scheduler,
+        # Start the Telemetry supervisor
+        FastApiWeb.Telemetry,
+        # Start the PubSub system
+        {Phoenix.PubSub, name: FastApi.PubSub},
+        # Start the Endpoint (http/https)
+        FastApiWeb.Endpoint
+        # Start a worker by calling: FastApi.Worker.start_link(arg)
+        # {FastApi.Worker, arg}
+      ])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: FastApi.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp prod_only_processes(true), do: [{Goth, name: FastApi.Goth}]
+  defp prod_only_processes(_), do: []
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
