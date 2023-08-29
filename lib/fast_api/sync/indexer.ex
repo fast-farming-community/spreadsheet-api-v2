04b17ef412b8730ce06e7fe711167969cfc14f21
Sam Wolfs <be.samwolfs@gmail.com>
2023-08-29 21:58:47 +0200

    Happens to the best of us

--- lib/fast_api/sync/indexer.ex
+++ lib/fast_api/sync/indexer.ex
@@ -2,14 +2,15 @@ defmodule FastApi.Sync.Indexer do
   alias FastApi.Repos.Fast, as: Repo
 
   def execute() do
-    index = Repo.Feature
-    |> Repo.all()
-    |> Repo.preload(pages: [:tables])
-    |> Enum.flat_map(fn %Repo.Feature{name: feature, pages: pages} ->
-      Enum.map(pages, fn %Repo.Page{name: page, tables: tables} ->
-        %{name: kebab_to_capital(page), route: "#{feature}/#{page}", tags: page_tags(tables)}
+    index =
+      Repo.Feature
+      |> Repo.all()
+      |> Repo.preload(pages: [:tables])
+      |> Enum.flat_map(fn %Repo.Feature{name: feature, pages: pages} ->
+        Enum.map(pages, fn %Repo.Page{name: page, tables: tables} ->
+          %{name: kebab_to_capital(page), route: "#{feature}/#{page}", tags: page_tags(tables)}
+        end)
       end)
-    end)
 
     Repo.Metadata
     |> Repo.get_by(name: "index")
@@ -18,12 +19,19 @@ defmodule FastApi.Sync.Indexer do
   end
 
   defp page_tags(tables) do
-    Enum.flat_map(tables, fn %Repo.Table{rows: rows} ->
-      rows
-      |> Jason.decode!()
-      |> tl()
-      |> Enum.map(&Map.get(&1, "Name"))
-    end)
+    Enum.flat_map(
+      tables,
+      fn
+        %Repo.Table{rows: "[]"} ->
+          []
+
+        %Repo.Table{rows: rows} ->
+          rows
+          |> Jason.decode!()
+          |> tl()
+          |> Enum.map(&Map.get(&1, "Name"))
+      end
+    )
   end
 
   defp kebab_to_capital(name),
