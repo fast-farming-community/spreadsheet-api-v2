356c343965aa120d08d7a70dfdcefb4e1cf7255b
Sam Wolfs <be.samwolfs@gmail.com>
2024-08-17 15:44:05 +0200

    Dynamic tiers

--- lib/fast_api/sync/patreon.ex
+++ lib/fast_api/sync/patreon.ex
@@ -5,9 +5,9 @@ defmodule FastApi.Sync.Patreon do
   def sync_memberships() do
     {:ok, members} = Client.active_patrons()
 
-    Enum.each(members, fn %{email: email} ->
+    Enum.each(members, fn %{email: email, role: role} ->
       with user <- Auth.get_user_by_email(email) do
-        Auth.set_role(user, "tribune")
+        Auth.set_role(user, role)
       end
     end)
   end
