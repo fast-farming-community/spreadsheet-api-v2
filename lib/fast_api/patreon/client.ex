c5f90a4bce81e8072cb52503f0a2052d7246b7d1
Sam Wolfs <be.samwolfs@gmail.com>
2024-08-25 18:24:02 +0200

    Handle Patreon API Pagination

--- lib/fast_api/patreon/client.ex
+++ lib/fast_api/patreon/client.ex
@@ -9,9 +9,24 @@ defmodule FastApi.Patreon.Client do
   }
 
   def active_patrons() do
+    "https://www.patreon.com/api/oauth2/v2/campaigns/#{Application.fetch_env!(:fast_api, :patreon_campaign)}/members?include=currently_entitled_tiers,address&fields%5Bmember%5D=email,is_follower,last_charge_date,last_charge_status,lifetime_support_cents,currently_entitled_amount_cents,patron_status&fields%5Btier%5D=title,amount_cents,created_at,edited_at,published,published_at,title"
+    |> get_patrons()
+    |> then(fn
+      {:ok, result} ->
+        patrons = Enum.flat_map(result.data, &build_patron/1)
+
+        active_patrons(result, patrons)
+
+      {:error, error} = e ->
+        Logger.error("Error while querying Patreon: #{error}")
+        e
+    end)
+  end
+
+  defp get_patrons(link) do
     :get
     |> Finch.build(
-      "https://www.patreon.com/api/oauth2/v2/campaigns/#{Application.fetch_env!(:fast_api, :patreon_campaign)}/members?include=currently_entitled_tiers,address&fields%5Bmember%5D=email,is_follower,last_charge_date,last_charge_status,lifetime_support_cents,currently_entitled_amount_cents,patron_status&fields%5Btier%5D=title,amount_cents,created_at,edited_at,published,published_at,title",
+      link,
       [
         {"Content-Type", "application/json"},
         {"Authorization", "Bearer #{Application.fetch_env!(:fast_api, :patreon_api_key)}"}
@@ -20,16 +35,28 @@ defmodule FastApi.Patreon.Client do
     |> Finch.request(FastApi.Finch)
     |> then(fn
       {:ok, %Finch.Response{body: body}} ->
-        body
-        |> Jason.decode!(keys: :atoms)
-        |> then(& &1.data)
-        |> Enum.flat_map(&build_patron/1)
-        |> then(&{:ok, &1})
+        {:ok, Jason.decode!(body, keys: :atoms)}
 
       {:error, error} = e ->
         Logger.error("Error while querying Patreon: #{error}")
         e
     end)
+  end
+
+  defp active_patrons(%{links: %{next: next}}, patrons \\ []) do
+    next
+    |> get_patrons()
+    |> then(fn
+      {:ok, result} ->
+        active_patrons(result, patrons ++ Enum.flat_map(result.data, &build_patron/1))
+
+      {:error, _} ->
+        patrons
+    end)
+  end
+
+  defp active_patrons(_, patrons) do
+    {:ok, patrons}
   end
 
   defp build_patron(%{
