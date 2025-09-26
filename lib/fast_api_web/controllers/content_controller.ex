ac45a7f5bcaa0e24128f512e3b7f3921670f0d86
Gerwin Rajkowski <gerwin@rajkowski.de>
2025-09-26 08:37:59 +0200

    fix catch error crash

--- lib/fast_api_web/controllers/content_controller.ex
+++ lib/fast_api_web/controllers/content_controller.ex
@@ -26,18 +26,60 @@ defmodule FastApiWeb.ContentController do
   end
 
   def changelog(conn, _params) do
-    data = github_file("CHANGELOG.md")
-    text(conn, data)
+    case github_file("CHANGELOG.md") do
+      {:ok, body} ->
+        text(conn, body)
+
+      {:error, {:upstream_timeout, msg}} ->
+        Logger.warning("changelog: upstream timeout: #{msg}")
+        send_resp(conn, 504, "Upstream timeout")
+
+      {:error, {:upstream_status, status, _body}} ->
+        Logger.warning("changelog: upstream status #{status}")
+        send_resp(conn, 502, "Upstream returned #{status}")
+
+      {:error, {:upstream_error, msg}} ->
+        Logger.error("changelog: upstream error: #{msg}")
+        send_resp(conn, 502, "Upstream error")
+    end
   end
 
   def content_updates(conn, _params) do
-    data = github_file("WEBSITE_CONTENT_UPDATES.md")
-    text(conn, data)
+    case github_file("WEBSITE_CONTENT_UPDATES.md") do
+      {:ok, body} ->
+        text(conn, body)
+
+      {:error, {:upstream_timeout, msg}} ->
+        Logger.warning("content_updates: upstream timeout: #{msg}")
+        send_resp(conn, 504, "Upstream timeout")
+
+      {:error, {:upstream_status, status, _body}} ->
+        Logger.warning("content_updates: upstream status #{status}")
+        send_resp(conn, 502, "Upstream returned #{status}")
+
+      {:error, {:upstream_error, msg}} ->
+        Logger.error("content_updates: upstream error: #{msg}")
+        send_resp(conn, 502, "Upstream error")
+    end
   end
 
   def todos(conn, _params) do
-    data = github_file("WEBSITE_TODOS.md")
-    text(conn, data)
+    case github_file("WEBSITE_TODOS.md") do
+      {:ok, body} ->
+        text(conn, body)
+
+      {:error, {:upstream_timeout, msg}} ->
+        Logger.warning("todos: upstream timeout: #{msg}")
+        send_resp(conn, 504, "Upstream timeout")
+
+      {:error, {:upstream_status, status, _body}} ->
+        Logger.warning("todos: upstream status #{status}")
+        send_resp(conn, 502, "Upstream returned #{status}")
+
+      {:error, {:upstream_error, msg}} ->
+        Logger.error("todos: upstream error: #{msg}")
+        send_resp(conn, 502, "Upstream error")
+    end
   end
 
   def contributors(conn, _params) do
@@ -59,20 +101,32 @@ defmodule FastApiWeb.ContentController do
     json(conn, data)
   end
 
-  def github_file(filename) do
-    :get
-    |> Finch.build(
-      "https://raw.githubusercontent.com/fast-farming-community/public/main/#{filename}",
-      [{"Content-Type", "text"}]
-    )
-    |> Finch.request(FastApi.Finch)
-    |> then(fn
-      {:ok, %Finch.Response{body: body}} ->
-        body
+  @github_raw_base "https://raw.githubusercontent.com/fast-farming-community/public/main/"
 
-      {:error, error} ->
-        Logger.error("Error requesting #{filename} from GitHub: #{error}")
-        ""
-    end)
+  @finch_timeout 12_000
+  @headers [{"accept", "text/plain"}]
+
+  @spec github_file(String.t()) ::
+          {:ok, binary()}
+          | {:error, {:upstream_timeout, String.t()}}
+          | {:error, {:upstream_status, non_neg_integer(), binary()}}
+          | {:error, {:upstream_error, String.t()}}
+  def github_file(filename) do
+    url = @github_raw_base <> filename
+    req = Finch.build(:get, url, @headers, nil)
+
+    case Finch.request(req, FastApi.Finch, receive_timeout: @finch_timeout) do
+      {:ok, %Finch.Response{status: 200, body: body}} ->
+        {:ok, body}
+
+      {:ok, %Finch.Response{status: status, body: body}} ->
+        {:error, {:upstream_status, status, body}}
+
+      {:error, %Mint.TransportError{} = err} ->
+        {:error, {:upstream_timeout, Exception.message(err)}}
+
+      {:error, err} ->
+        {:error, {:upstream_error, Exception.message(err)}}
+    end
   end
 end
