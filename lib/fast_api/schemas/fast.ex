f3dd0f02c6631477aa4182cbdca36827556734a3
Gerwin Rajkowski <gerwin@rajkowski.de>
2025-10-01 10:58:00 +0200

    add order-sensitive schema

--- lib/fast_api/schemas/fast.ex
+++ lib/fast_api/schemas/fast.ex
@@ -4,12 +4,14 @@ defmodule FastApi.Schemas.Fast do
   defmodule About do
     @moduledoc false
     use Ecto.Schema
+
     @derive {Jason.Encoder, only: [:content, :published, :title]}
     schema "about" do
       field(:content, :string)
       field(:order, :integer)
       field(:published, :boolean)
       field(:title, :string)
+
       timestamps()
     end
   end
@@ -17,11 +19,26 @@ defmodule FastApi.Schemas.Fast do
   defmodule Build do
     @moduledoc false
     use Ecto.Schema
+
     @derive {Jason.Encoder,
              only: [
-               :armor, :burstRotation, :multiTarget, :name, :notice, :overview, :profession,
-               :published, :singleTarget, :skills, :specialization, :template,
-               :traits, :traitsInfo, :trinkets, :utilitySkills, :weapons
+               :armor,
+               :burstRotation,
+               :multiTarget,
+               :name,
+               :notice,
+               :overview,
+               :profession,
+               :published,
+               :singleTarget,
+               :skills,
+               :specialization,
+               :template,
+               :traits,
+               :traitsInfo,
+               :trinkets,
+               :utilitySkills,
+               :weapons
              ]}
     schema "builds" do
       field(:armor, :string)
@@ -41,6 +58,7 @@ defmodule FastApi.Schemas.Fast do
       field(:trinkets, :string)
       field(:utilitySkills, :string)
       field(:weapons, :string)
+
       timestamps()
     end
   end
@@ -48,11 +66,70 @@ defmodule FastApi.Schemas.Fast do
   defmodule Contributor do
     @moduledoc false
     use Ecto.Schema
+
     @derive {Jason.Encoder, only: [:name, :type]}
     schema "contributors" do
       field(:name, :string)
       field(:published, :boolean)
       field(:type, :string)
+
+      timestamps()
+    end
+  end
+
+  defmodule DetailFeature do
+    @moduledoc false
+    use Ecto.Schema
+
+    @derive {Jason.Encoder, only: [:name, :detail_tables]}
+    schema "detail_features" do
+      field(:name, :string)
+      has_many(:detail_tables, FastApi.Schemas.Fast.DetailTable)
+      field(:published, :boolean)
+
+      timestamps()
+    end
+  end
+
+  defmodule DetailTable do
+    @moduledoc false
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @derive {Jason.Encoder, only: [:description, :key, :name, :rows]}
+    schema "detail_tables" do
+      field(:description, :string)
+      belongs_to(:detail_feature, FastApi.Schemas.Fast.DetailFeature)
+      field(:key, :string)
+      field(:name, :string)
+      field(:range, :string)
+      field(:rows, :string)          # legacy
+
+      # 🔹 NEW tiered payloads
+      field(:rows_copper, :string)
+      field(:rows_silver, :string)
+      field(:rows_gold,   :string)
+
+      timestamps()
+    end
+
+    def changeset(table, params \\ %{}) do
+      table
+      |> cast(params, [:rows, :rows_copper, :rows_silver, :rows_gold])
+      |> unique_constraint(:detail_tables_unique_id)
+    end
+  end
+
+  defmodule Feature do
+    @moduledoc false
+    use Ecto.Schema
+
+    @derive {Jason.Encoder, only: [:name, :pages]}
+    schema "features" do
+      field(:name, :string)
+      has_many(:pages, FastApi.Schemas.Fast.Page)
+      field(:published, :boolean)
+
       timestamps()
     end
   end
@@ -60,6 +137,7 @@ defmodule FastApi.Schemas.Fast do
   defmodule Guide do
     @moduledoc false
     use Ecto.Schema
+
     @derive {Jason.Encoder, only: [:farmtrain, :image, :info, :published, :title]}
     schema "guides" do
       field(:farmtrain, :string)
@@ -68,6 +146,7 @@ defmodule FastApi.Schemas.Fast do
       field(:order, :integer)
       field(:published, :boolean)
       field(:title, :string)
+
       timestamps()
     end
   end
@@ -89,6 +168,7 @@ defmodule FastApi.Schemas.Fast do
       field(:tradable, :boolean)
       field(:type, :string)
       field(:vendor_value, :integer)
+
       timestamps()
     end
 
@@ -105,10 +185,10 @@ defmodule FastApi.Schemas.Fast do
     Store JSON blobs (data) containing website metadata
 
     Default metadata (name):
-      - main:    per-tier `updated_at` for feature sync
-      - detail:  per-tier `updated_at` for detail sync
-      - index:   table index
-      - public:  CHANGELOG/WEBSITE_CONTENT_UPDATES/WEBSITE_TODOS timestamps
+      - main: Contains `updated_at` time for feature sync
+      - detail: Contains `updated_at` time for detail sync
+      - index: Contains the table index
+      - public: Contains CHANGELOG, WEBSITE_CONTENT_UPDATES and WEBSITE_TODOS update times
     """
     use Ecto.Schema
     import Ecto.Changeset
@@ -117,23 +197,24 @@ defmodule FastApi.Schemas.Fast do
     schema "metadata" do
       field(:data, :string)
       field(:name, :string)
+
       timestamps()
     end
 
     def changeset(metadata, params \\ %{}), do: cast(metadata, params, [:data])
   end
 
-  defmodule Feature do
+  defmodule Page do
     @moduledoc false
     use Ecto.Schema
 
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.Page}
-
-    @derive {Jason.Encoder, only: [:name, :pages]}
-    schema "features" do
+    @derive {Jason.Encoder, only: [:name, :tables]}
+    schema "pages" do
+      belongs_to(:feature, FastApi.Schemas.Fast.Feature)
       field(:name, :string)
-      has_many(:pages, FastApi.Schemas.Fast.Page)
       field(:published, :boolean)
+      has_many(:tables, FastApi.Schemas.Fast.Table)
+
       timestamps()
     end
   end
@@ -143,8 +224,6 @@ defmodule FastApi.Schemas.Fast do
     use Ecto.Schema
     import Ecto.Changeset
 
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.Page}
-
     @derive {Jason.Encoder, only: [:description, :name, :order, :rows, :restrictions]}
     schema "tables" do
       field :description, :string
@@ -153,14 +232,14 @@ defmodule FastApi.Schemas.Fast do
       belongs_to(:page, FastApi.Schemas.Fast.Page)
       field :published, :boolean
       field :range, :string
+      field :rows, :string          # legacy
 
-      # legacy + new tiered payloads
-      field :rows, :string
       field :rows_copper, :string
       field :rows_silver, :string
-      field :rows_gold, :string
+      field :rows_gold,   :string
 
       field :restrictions, :map, virtual: true
+
       timestamps()
     end
 
@@ -168,69 +247,6 @@ defmodule FastApi.Schemas.Fast do
       table
       |> cast(params, [:rows, :rows_copper, :rows_silver, :rows_gold])
       |> unique_constraint(:tables_unique_id)
-    end
-  end
-
-  defmodule DetailTable do
-    @moduledoc false
-    use Ecto.Schema
-    import Ecto.Changeset
-
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.DetailFeature}
-
-    @derive {Jason.Encoder, only: [:description, :key, :name, :rows]}
-    schema "detail_tables" do
-      field(:description, :string)
-      belongs_to(:detail_feature, FastApi.Schemas.Fast.DetailFeature)
-      field(:key, :string)
-      field(:name, :string)
-      field(:range, :string)
-
-      # legacy + new tiered payloads
-      field(:rows, :string)
-      field(:rows_copper, :string)
-      field(:rows_silver, :string)
-      field(:rows_gold, :string)
-
-      timestamps()
-    end
-
-    def changeset(table, params \\ %{}) do
-      table
-      |> cast(params, [:rows, :rows_copper, :rows_silver, :rows_gold])
-      |> unique_constraint(:detail_tables_unique_id)
-    end
-  end
-
-  defmodule DetailFeature do
-    @moduledoc false
-    use Ecto.Schema
-
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.DetailTable}
-
-    @derive {Jason.Encoder, only: [:name, :detail_tables]}
-    schema "detail_features" do
-      field(:name, :string)
-      has_many(:detail_tables, FastApi.Schemas.Fast.DetailTable)
-      field(:published, :boolean)
-      timestamps()
-    end
-  end
-
-  defmodule Page do
-    @moduledoc false
-    use Ecto.Schema
-
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.Feature}
-    @compile {:no_warn_undefined, FastApi.Schemas.Fast.Table}
-
-    @derive {Jason.Encoder, only: [:name, :tables]}
-    schema "pages" do
-      belongs_to(:feature, FastApi.Schemas.Fast.Feature)
-      field(:name, :string)
-      field(:published, :boolean)
-      has_many(:tables, FastApi.Schemas.Fast.Table)
-      timestamps()
     end
   end
 end
