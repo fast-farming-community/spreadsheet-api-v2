f7ec0b86119c8ebf637fe402d2c27ec9a7035189
Gerwin Rajkowski <gerwin@rajkowski.de>
2025-10-01 18:00:44 +0200

    fix public and secure endpoints

--- lib/fast_api_web/controllers/meta_controller.ex
+++ lib/fast_api_web/controllers/meta_controller.ex
@@ -6,40 +6,46 @@ defmodule FastApiWeb.MetaController do
 
   def index(conn, _params) do
     tier_key =
-      conn.assigns[:tier]
-      |> case do
+      case conn.assigns[:tier] do
         nil -> "free"
         a when is_atom(a) -> Atom.to_string(a)
         b when is_binary(b) -> b
         _ -> "free"
       end
 
-    Fast.Metadata
-    |> Repo.all()
-    |> Enum.map(fn
-      %Fast.Metadata{name: name, data: nil} ->
-        %{name: name, data: %{}, updated_at: nil}
+    metas =
+      Fast.Metadata
+      |> Repo.all()
+      |> Enum.map(&safe_encode(&1, tier_key))
 
-      %Fast.Metadata{name: name, data: ""} ->
-        %{name: name, data: %{}, updated_at: nil}
+    json(conn, metas)
+  end
 
-      %Fast.Metadata{name: name, data: json} ->
-        case Jason.decode(json) do
-          {:ok, decoded} ->
-            updated_at =
-              decoded
-              |> Map.get("updated_at", %{})
-              |> case do
-                m when is_map(m) -> Map.get(m, tier_key)
-                _ -> nil
-              end
+  defp safe_encode(%Fast.Metadata{name: name, data: data}, tier_key) do
+    decoded =
+      case data do
+        nil -> %{}
+        "" -> %{}
+        bin when is_binary(bin) ->
+          case Jason.decode(bin) do
+            {:ok, m} when is_map(m) -> m
+            _ -> %{}
+          end
+        _ -> %{}
+      end
 
-            %{name: name, data: decoded, updated_at: updated_at}
+    updated_at =
+      decoded
+      |> Map.get("updated_at", %{})
+      |> case do
+        m when is_map(m) -> Map.get(m, tier_key)
+        _ -> nil
+      end
 
-          _ ->
-            %{name: name, data: %{}, updated_at: nil}
-        end
-    end)
-    |> then(&json(conn, &1))
+    %{
+      name: name,
+      data: decoded,
+      updated_at: updated_at
+    }
   end
 end
