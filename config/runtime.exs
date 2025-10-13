0f63b18530ece5a532799891ec4b5230ebcc6fa2
Gerwin Rajkowski <gerwin@rajkowski.de>
2025-10-14 00:18:12 +0200

    fix CORS 503 errors

--- config/runtime.exs
+++ config/runtime.exs
@@ -19,11 +19,7 @@ if config_env() == :prod do
     url: [host: host, port: 443, scheme: "https"],
     http: [
       ip: {0,0,0,0,0,0,0,0},
-      port: port,
-      protocol_options: [
-        request_timeout: 30_000,
-        idle_timeout: 30_000
-      ]
+      port: port
     ],
     secret_key_base: secret_key_base
 
