766dbe3aa5b230d8f4d3c9544a24ad2833d2bbdb
Gerwin Rajkowski <gerwin@rajkowski.de>
2025-10-13 23:46:33 +0200

    disable SSE for now

--- config/prod.exs
+++ config/prod.exs
@@ -3,16 +3,7 @@ import Config
 config :fast_api, FastApiWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [
-      num_acceptors: 10,
-      max_connections: 1024
-    ],
-    protocol_options: [
-      idle_timeout: 30_000,
-      request_timeout: 15_000,
-      max_keepalive: 50
-    ]
+    port: String.to_integer(System.get_env("PORT") || "4000")
   ]
 
 config :logger, level: :info
